03dee496bc6a74d7396cc49334a3b7e898520951
samgomena <samiam4612@gmail.com>
2020-03-09 09:25:05 -0700

    feat: Basic outline of project

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+struct VXI11 {
+    link: Option<usize>,
+    client: usize,
+}
+
+impl VXI11 {
+    fn open(self) -> Option<bool> {
+        if self.link.is_none() {
+            None
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
