9c74b98adedc49625609abbdc13bd939bdda4f8a
Sam Gomena <sgomena@tripwire.com>
2020-03-13 00:32:05 -0700

    feat: Very rough standalone implementation; also doesn't compile

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+// extern crate serde_xdr;
+
+#[macro_use]
+extern crate serde_derive;
+extern crate tokio_core;
+extern crate tokio_proto;
+extern crate tokio_service;
+
+use tokio_proto::TcpServer;
+
+mod codec;
+mod proto;
+mod service;
+mod xdr_codec;
+mod xdr_rpc;
+
+use codec::*;
+use service::*;
+
+fn main() {
+    println!("Hello, World")
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
