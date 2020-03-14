ba69ac5ac5a912ad12ead6142e268c51b354a2c3
Sam Gomena <sgomena@tripwire.com>
2020-03-13 19:38:26 -0700

    feat: Format with cargo

--- src/codec.rs
+++ src/codec.rs
@@ -12,114 +12,114 @@ use tokio_proto::pipeline::ServerProto;
 
 pub struct DeviceAsyncCodec;
 impl Codec for DeviceAsyncCodec {
-  type In = DeviceAsyncRequest;
-  type Out = DeviceAsyncResponse;
-  fn decode(&mut self, _buf: &mut EasyBuf) -> io::Result<Option<Self::In>> {
-    unreachable!()
-  }
-  fn encode(&mut self, msg: Self::Out, buf: &mut Vec<u8>) -> io::Result<()> {
-    encode_device_async(msg, buf)
-  }
+    type In = DeviceAsyncRequest;
+    type Out = DeviceAsyncResponse;
+    fn decode(&mut self, _buf: &mut EasyBuf) -> io::Result<Option<Self::In>> {
+        unreachable!()
+    }
+    fn encode(&mut self, msg: Self::Out, buf: &mut Vec<u8>) -> io::Result<()> {
+        encode_device_async(msg, buf)
+    }
 }
 impl AppCodec for DeviceAsyncCodec {
-  fn app_decode(
-    &mut self,
-    prog: u32,
-    version: u32,
-    procedure: u32,
-    buf: &mut EasyBuf,
-  ) -> io::Result<Option<Self::In>> {
-    device_async_decode(prog, version, procedure, buf)
-  }
+    fn app_decode(
+        &mut self,
+        prog: u32,
+        version: u32,
+        procedure: u32,
+        buf: &mut EasyBuf,
+    ) -> io::Result<Option<Self::In>> {
+        device_async_decode(prog, version, procedure, buf)
+    }
 }
 pub type DeviceAsync = XdrCodec<DeviceAsyncCodec>;
 
 pub struct DeviceAsyncProtocol;
 impl<T> ServerProto<T> for DeviceAsyncProtocol
 where
-  T: Io + 'static,
+    T: Io + 'static,
 {
-  type Request = xdr_rpc::XdrRequest<DeviceAsyncRequest>;
-  type Response = xdr_rpc::XdrResponse<DeviceAsyncResponse>;
-  type Transport = Framed<T, DeviceAsync>;
-  type BindTransport = io::Result<Self::Transport>;
-  fn bind_transport(&self, io: T) -> Self::BindTransport {
-    Ok(io.framed(DeviceAsync::new(DeviceAsyncCodec)))
-  }
+    type Request = xdr_rpc::XdrRequest<DeviceAsyncRequest>;
+    type Response = xdr_rpc::XdrResponse<DeviceAsyncResponse>;
+    type Transport = Framed<T, DeviceAsync>;
+    type BindTransport = io::Result<Self::Transport>;
+    fn bind_transport(&self, io: T) -> Self::BindTransport {
+        Ok(io.framed(DeviceAsync::new(DeviceAsyncCodec)))
+    }
 }
 
 pub struct DeviceCoreCodec;
 impl Codec for DeviceCoreCodec {
-  type In = DeviceCoreRequest;
-  type Out = DeviceCoreResponse;
-  fn decode(&mut self, _buf: &mut EasyBuf) -> io::Result<Option<Self::In>> {
-    unreachable!()
-  }
-  fn encode(&mut self, msg: Self::Out, buf: &mut Vec<u8>) -> io::Result<()> {
-    encode_device_core(msg, buf)
-  }
+    type In = DeviceCoreRequest;
+    type Out = DeviceCoreResponse;
+    fn decode(&mut self, _buf: &mut EasyBuf) -> io::Result<Option<Self::In>> {
+        unreachable!()
+    }
+    fn encode(&mut self, msg: Self::Out, buf: &mut Vec<u8>) -> io::Result<()> {
+        encode_device_core(msg, buf)
+    }
 }
 impl AppCodec for DeviceCoreCodec {
-  fn app_decode(
-    &mut self,
-    prog: u32,
-    version: u32,
-    procedure: u32,
-    buf: &mut EasyBuf,
-  ) -> io::Result<Option<Self::In>> {
-    device_core_decode(prog, version, procedure, buf)
-  }
+    fn app_decode(
+        &mut self,
+        prog: u32,
+        version: u32,
+        procedure: u32,
+        buf: &mut EasyBuf,
+    ) -> io::Result<Option<Self::In>> {
+        device_core_decode(prog, version, procedure, buf)
+    }
 }
 pub type DeviceCore = XdrCodec<DeviceCoreCodec>;
 
 pub struct DeviceCoreProtocol;
 impl<T> ServerProto<T> for DeviceCoreProtocol
 where
-  T: Io + 'static,
+    T: Io + 'static,
 {
-  type Request = xdr_rpc::XdrRequest<DeviceCoreRequest>;
-  type Response = xdr_rpc::XdrResponse<DeviceCoreResponse>;
-  type Transport = Framed<T, DeviceCore>;
-  type BindTransport = io::Result<Self::Transport>;
-  fn bind_transport(&self, io: T) -> Self::BindTransport {
-    Ok(io.framed(DeviceCore::new(DeviceCoreCodec)))
-  }
+    type Request = xdr_rpc::XdrRequest<DeviceCoreRequest>;
+    type Response = xdr_rpc::XdrResponse<DeviceCoreResponse>;
+    type Transport = Framed<T, DeviceCore>;
+    type BindTransport = io::Result<Self::Transport>;
+    fn bind_transport(&self, io: T) -> Self::BindTransport {
+        Ok(io.framed(DeviceCore::new(DeviceCoreCodec)))
+    }
 }
 
 pub struct DeviceIntrCodec;
 impl Codec for DeviceIntrCodec {
-  type In = DeviceIntrRequest;
-  type Out = DeviceIntrResponse;
-  fn decode(&mut self, _buf: &mut EasyBuf) -> io::Result<Option<Self::In>> {
-    unreachable!()
-  }
-  fn encode(&mut self, msg: Self::Out, buf: &mut Vec<u8>) -> io::Result<()> {
-    encode_device_intr(msg, buf)
-  }
+    type In = DeviceIntrRequest;
+    type Out = DeviceIntrResponse;
+    fn decode(&mut self, _buf: &mut EasyBuf) -> io::Result<Option<Self::In>> {
+        unreachable!()
+    }
+    fn encode(&mut self, msg: Self::Out, buf: &mut Vec<u8>) -> io::Result<()> {
+        encode_device_intr(msg, buf)
+    }
 }
 impl AppCodec for DeviceIntrCodec {
-  fn app_decode(
-    &mut self,
-    prog: u32,
-    version: u32,
-    procedure: u32,
-    buf: &mut EasyBuf,
-  ) -> io::Result<Option<Self::In>> {
-    device_intr_decode(prog, version, procedure, buf)
-  }
+    fn app_decode(
+        &mut self,
+        prog: u32,
+        version: u32,
+        procedure: u32,
+        buf: &mut EasyBuf,
+    ) -> io::Result<Option<Self::In>> {
+        device_intr_decode(prog, version, procedure, buf)
+    }
 }
 pub type DeviceIntr = XdrCodec<DeviceIntrCodec>;
 
 pub struct DeviceIntrProtocol;
 impl<T> ServerProto<T> for DeviceIntrProtocol
 where
-  T: Io + 'static,
+    T: Io + 'static,
 {
-  type Request = xdr_rpc::XdrRequest<DeviceIntrRequest>;
-  type Response = xdr_rpc::XdrResponse<DeviceIntrResponse>;
-  type Transport = Framed<T, DeviceIntr>;
-  type BindTransport = io::Result<Self::Transport>;
-  fn bind_transport(&self, io: T) -> Self::BindTransport {
-    Ok(io.framed(DeviceIntr::new(DeviceIntrCodec)))
-  }
+    type Request = xdr_rpc::XdrRequest<DeviceIntrRequest>;
+    type Response = xdr_rpc::XdrResponse<DeviceIntrResponse>;
+    type Transport = Framed<T, DeviceIntr>;
+    type BindTransport = io::Result<Self::Transport>;
+    fn bind_transport(&self, io: T) -> Self::BindTransport {
+        Ok(io.framed(DeviceIntr::new(DeviceIntrCodec)))
+    }
 }
--- src/service.rs
+++ src/service.rs
@@ -15,146 +15,155 @@ use std::io;
 use tokio_service::Service;
 
 impl<'a> Service for DeviceAsyncService<'a> {
-  type Request = xdr_rpc::XdrRequest<DeviceAsyncRequest>;
-  type Response = xdr_rpc::XdrResponse<DeviceAsyncResponse>;
-  type Error = io::Error;
-  type Future = BoxFuture<Self::Response, Self::Error>;
-  fn call(&self, req: Self::Request) -> Self::Future {
-    let xid = req.xid;
-    match req.val {
-      DeviceAsyncRequest::V1(data) => {
-        let res = match data {
-          DeviceAsyncRequestV1::DeviceAbort(arg0) => self
-            .device_abort_v1(arg0)
-            .map(DeviceAsyncResponseV1::DeviceAbort)
-            .boxed(),
-          _ => {
-            return future::err(io::Error::new(io::ErrorKind::Other, "unknown procedure")).boxed();
-          }
-        };
-        res
-          .map(move |r| xdr_rpc::XdrResponse {
-            xid,
-            val: DeviceAsyncResponse::V1(r),
-          })
-          .boxed()
-      }
-      _ => future::err(io::Error::new(io::ErrorKind::Other, "unknown version")).boxed(),
+    type Request = xdr_rpc::XdrRequest<DeviceAsyncRequest>;
+    type Response = xdr_rpc::XdrResponse<DeviceAsyncResponse>;
+    type Error = io::Error;
+    type Future = BoxFuture<Self::Response, Self::Error>;
+    fn call(&self, req: Self::Request) -> Self::Future {
+        let xid = req.xid;
+        match req.val {
+            DeviceAsyncRequest::V1(data) => {
+                let res = match data {
+                    DeviceAsyncRequestV1::DeviceAbort(arg0) => self
+                        .device_abort_v1(arg0)
+                        .map(DeviceAsyncResponseV1::DeviceAbort)
+                        .boxed(),
+                    _ => {
+                        return future::err(io::Error::new(
+                            io::ErrorKind::Other,
+                            "unknown procedure",
+                        ))
+                        .boxed();
+                    }
+                };
+                res.map(move |r| xdr_rpc::XdrResponse {
+                    xid,
+                    val: DeviceAsyncResponse::V1(r),
+                })
+                .boxed()
+            }
+            _ => future::err(io::Error::new(io::ErrorKind::Other, "unknown version")).boxed(),
+        }
     }
-  }
 }
 impl<'a> Service for DeviceCoreService<'a> {
-  type Request = xdr_rpc::XdrRequest<DeviceCoreRequest>;
-  type Response = xdr_rpc::XdrResponse<DeviceCoreResponse>;
-  type Error = io::Error;
-  type Future = BoxFuture<Self::Response, Self::Error>;
-  fn call(&self, req: Self::Request) -> Self::Future {
-    let xid = req.xid;
-    match req.val {
-      DeviceCoreRequest::V1(data) => {
-        let res = match data {
-          DeviceCoreRequestV1::CreateLink(arg0) => self
-            .create_link_v1(arg0)
-            .map(DeviceCoreResponseV1::CreateLink)
-            .boxed(),
-          DeviceCoreRequestV1::DeviceWrite(arg0) => self
-            .device_write_v1(arg0)
-            .map(DeviceCoreResponseV1::DeviceWrite)
-            .boxed(),
-          DeviceCoreRequestV1::DeviceRead(arg0) => self
-            .device_read_v1(arg0)
-            .map(DeviceCoreResponseV1::DeviceRead)
-            .boxed(),
-          DeviceCoreRequestV1::DeviceReadstb(arg0) => self
-            .device_readstb_v1(arg0)
-            .map(DeviceCoreResponseV1::DeviceReadstb)
-            .boxed(),
-          DeviceCoreRequestV1::DeviceTrigger(arg0) => self
-            .device_trigger_v1(arg0)
-            .map(DeviceCoreResponseV1::DeviceTrigger)
-            .boxed(),
-          DeviceCoreRequestV1::DeviceClear(arg0) => self
-            .device_clear_v1(arg0)
-            .map(DeviceCoreResponseV1::DeviceClear)
-            .boxed(),
-          DeviceCoreRequestV1::DeviceRemote(arg0) => self
-            .device_remote_v1(arg0)
-            .map(DeviceCoreResponseV1::DeviceRemote)
-            .boxed(),
-          DeviceCoreRequestV1::DeviceLocal(arg0) => self
-            .device_local_v1(arg0)
-            .map(DeviceCoreResponseV1::DeviceLocal)
-            .boxed(),
-          DeviceCoreRequestV1::DeviceLock(arg0) => self
-            .device_lock_v1(arg0)
-            .map(DeviceCoreResponseV1::DeviceLock)
-            .boxed(),
-          DeviceCoreRequestV1::DeviceUnlock(arg0) => self
-            .device_unlock_v1(arg0)
-            .map(DeviceCoreResponseV1::DeviceUnlock)
-            .boxed(),
-          DeviceCoreRequestV1::DeviceEnableSrq(arg0) => self
-            .device_enable_srq_v1(arg0)
-            .map(DeviceCoreResponseV1::DeviceEnableSrq)
-            .boxed(),
-          DeviceCoreRequestV1::DeviceDocmd(arg0) => self
-            .device_docmd_v1(arg0)
-            .map(DeviceCoreResponseV1::DeviceDocmd)
-            .boxed(),
-          DeviceCoreRequestV1::DestroyLink(arg0) => self
-            .destroy_link_v1(arg0)
-            .map(DeviceCoreResponseV1::DestroyLink)
-            .boxed(),
-          DeviceCoreRequestV1::CreateIntrChan(arg0) => self
-            .create_intr_chan_v1(arg0)
-            .map(DeviceCoreResponseV1::CreateIntrChan)
-            .boxed(),
-          DeviceCoreRequestV1::DestroyIntrChan => self
-            .destroy_intr_chan_v1()
-            .map(DeviceCoreResponseV1::DestroyIntrChan)
-            .boxed(),
-          _ => {
-            return future::err(io::Error::new(io::ErrorKind::Other, "unknown procedure")).boxed();
-          }
-        };
-        res
-          .map(move |r| xdr_rpc::XdrResponse {
-            xid,
-            val: DeviceCoreResponse::V1(r),
-          })
-          .boxed()
-      }
-      _ => future::err(io::Error::new(io::ErrorKind::Other, "unknown version")).boxed(),
+    type Request = xdr_rpc::XdrRequest<DeviceCoreRequest>;
+    type Response = xdr_rpc::XdrResponse<DeviceCoreResponse>;
+    type Error = io::Error;
+    type Future = BoxFuture<Self::Response, Self::Error>;
+    fn call(&self, req: Self::Request) -> Self::Future {
+        let xid = req.xid;
+        match req.val {
+            DeviceCoreRequest::V1(data) => {
+                let res = match data {
+                    DeviceCoreRequestV1::CreateLink(arg0) => self
+                        .create_link_v1(arg0)
+                        .map(DeviceCoreResponseV1::CreateLink)
+                        .boxed(),
+                    DeviceCoreRequestV1::DeviceWrite(arg0) => self
+                        .device_write_v1(arg0)
+                        .map(DeviceCoreResponseV1::DeviceWrite)
+                        .boxed(),
+                    DeviceCoreRequestV1::DeviceRead(arg0) => self
+                        .device_read_v1(arg0)
+                        .map(DeviceCoreResponseV1::DeviceRead)
+                        .boxed(),
+                    DeviceCoreRequestV1::DeviceReadstb(arg0) => self
+                        .device_readstb_v1(arg0)
+                        .map(DeviceCoreResponseV1::DeviceReadstb)
+                        .boxed(),
+                    DeviceCoreRequestV1::DeviceTrigger(arg0) => self
+                        .device_trigger_v1(arg0)
+                        .map(DeviceCoreResponseV1::DeviceTrigger)
+                        .boxed(),
+                    DeviceCoreRequestV1::DeviceClear(arg0) => self
+                        .device_clear_v1(arg0)
+                        .map(DeviceCoreResponseV1::DeviceClear)
+                        .boxed(),
+                    DeviceCoreRequestV1::DeviceRemote(arg0) => self
+                        .device_remote_v1(arg0)
+                        .map(DeviceCoreResponseV1::DeviceRemote)
+                        .boxed(),
+                    DeviceCoreRequestV1::DeviceLocal(arg0) => self
+                        .device_local_v1(arg0)
+                        .map(DeviceCoreResponseV1::DeviceLocal)
+                        .boxed(),
+                    DeviceCoreRequestV1::DeviceLock(arg0) => self
+                        .device_lock_v1(arg0)
+                        .map(DeviceCoreResponseV1::DeviceLock)
+                        .boxed(),
+                    DeviceCoreRequestV1::DeviceUnlock(arg0) => self
+                        .device_unlock_v1(arg0)
+                        .map(DeviceCoreResponseV1::DeviceUnlock)
+                        .boxed(),
+                    DeviceCoreRequestV1::DeviceEnableSrq(arg0) => self
+                        .device_enable_srq_v1(arg0)
+                        .map(DeviceCoreResponseV1::DeviceEnableSrq)
+                        .boxed(),
+                    DeviceCoreRequestV1::DeviceDocmd(arg0) => self
+                        .device_docmd_v1(arg0)
+                        .map(DeviceCoreResponseV1::DeviceDocmd)
+                        .boxed(),
+                    DeviceCoreRequestV1::DestroyLink(arg0) => self
+                        .destroy_link_v1(arg0)
+                        .map(DeviceCoreResponseV1::DestroyLink)
+                        .boxed(),
+                    DeviceCoreRequestV1::CreateIntrChan(arg0) => self
+                        .create_intr_chan_v1(arg0)
+                        .map(DeviceCoreResponseV1::CreateIntrChan)
+                        .boxed(),
+                    DeviceCoreRequestV1::DestroyIntrChan => self
+                        .destroy_intr_chan_v1()
+                        .map(DeviceCoreResponseV1::DestroyIntrChan)
+                        .boxed(),
+                    _ => {
+                        return future::err(io::Error::new(
+                            io::ErrorKind::Other,
+                            "unknown procedure",
+                        ))
+                        .boxed();
+                    }
+                };
+                res.map(move |r| xdr_rpc::XdrResponse {
+                    xid,
+                    val: DeviceCoreResponse::V1(r),
+                })
+                .boxed()
+            }
+            _ => future::err(io::Error::new(io::ErrorKind::Other, "unknown version")).boxed(),
+        }
     }
-  }
 }
 
 impl<'a> Service for DeviceIntrService<'a> {
-  type Request = xdr_rpc::XdrRequest<DeviceIntrRequest>;
-  type Response = xdr_rpc::XdrResponse<DeviceIntrResponse>;
-  type Error = io::Error;
-  type Future = BoxFuture<Self::Response, Self::Error>;
-  fn call(&self, req: Self::Request) -> Self::Future {
-    let xid = req.xid;
-    match req.val {
-      DeviceIntrRequest::V1(data) => {
-        let res = match data {
-          DeviceIntrRequestV1::DeviceIntrSrq(arg0) => self
-            .device_intr_srq_v1(arg0)
-            .map(|_r| DeviceIntrResponseV1::DeviceIntrSrq)
-            .boxed(),
-          _ => {
-            return future::err(io::Error::new(io::ErrorKind::Other, "unknown procedure")).boxed()
-          }
-        };
-        res
-          .map(move |r| xdr_rpc::XdrResponse {
-            xid,
-            val: DeviceIntrResponse::V1(r),
-          })
-          .boxed()
-      }
-      _ => future::err(io::Error::new(io::ErrorKind::Other, "unknown version")).boxed(),
+    type Request = xdr_rpc::XdrRequest<DeviceIntrRequest>;
+    type Response = xdr_rpc::XdrResponse<DeviceIntrResponse>;
+    type Error = io::Error;
+    type Future = BoxFuture<Self::Response, Self::Error>;
+    fn call(&self, req: Self::Request) -> Self::Future {
+        let xid = req.xid;
+        match req.val {
+            DeviceIntrRequest::V1(data) => {
+                let res = match data {
+                    DeviceIntrRequestV1::DeviceIntrSrq(arg0) => self
+                        .device_intr_srq_v1(arg0)
+                        .map(|_r| DeviceIntrResponseV1::DeviceIntrSrq)
+                        .boxed(),
+                    _ => {
+                        return future::err(io::Error::new(
+                            io::ErrorKind::Other,
+                            "unknown procedure",
+                        ))
+                        .boxed()
+                    }
+                };
+                res.map(move |r| xdr_rpc::XdrResponse {
+                    xid,
+                    val: DeviceIntrResponse::V1(r),
+                })
+                .boxed()
+            }
+            _ => future::err(io::Error::new(io::ErrorKind::Other, "unknown version")).boxed(),
+        }
     }
-  }
 }
